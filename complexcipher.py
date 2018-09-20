fd91008556991c60c88698b1773d17c133322d6f
Om_G <im.omgupta@gmail.com>
2018-09-20 17:00:53 +0530

    Removed unneccessary prompt asking user whether they want to continue.

--- complexcipher.py
+++ complexcipher.py
@@ -3,9 +3,7 @@ from complexciphercore import convert
 
 print("Welcome to ComplexCipher v1.4.1 by Om Gupta!")
 
-quit_wish = "Y"
-
-while quit_wish == "Y":
+while True:
   while True:
     type = (input("Enter function to be performed. (Encode/Decode): ").lower()).strip()
     if type == "encode" or type == "decode" or type == "e" or type == "d":
@@ -25,15 +23,3 @@ while quit_wish == "Y":
   print(convert(text,type))
 
   print("")
-
-  while True:
-    quit_wish = input("Do you want to continue? (Y/N): ").upper()
-    if quit_wish == "Y" or quit_wish == "N":
-      break
-    else:
-      print("")
-      print("Sorry, that doesn't make sense.")
-
-print("")
-
-end_stop = input("Press enter to quit.")
