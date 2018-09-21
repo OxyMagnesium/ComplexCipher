6a89d03499ef2395b83751a796a1e5da703ad625
Om_G <im.omgupta@gmail.com>
2018-09-21 16:28:33 +0530

    Added direct message functionality, added automatic token detection, fixed minor bugs.

--- cipherinterpreterbot.py
+++ cipherinterpreterbot.py
@@ -5,8 +5,18 @@ import time
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = ''
-dest_channel = '491938711693426688' #secret-codes
+token = ''
+server_channel = '491938711693426688' #secret-codes
+
+if token == '':
+    try:
+        file = open('token.txt')
+        token = file.read()
+        file.close()
+    except FileNotFoundError:
+        print("Token not found. Please check that the file is correctly named or add the token directly into the code.")
+        end_stop = input("Press enter to quit.")
+        quit()
 
 client = discord.Client()
 
@@ -22,30 +32,45 @@ async def on_message(message):
 
     print("\n(@%s) Checking message by %s in %s..." % (msgtime, message.author, channel))
 
+    if channel.is_private == True:
+        dest_channel = channel.id
+    else:
+        dest_channel = server_channel
+
     try:
         if content.startswith('!e'):
             print("Ident as encode request.")
             content = (content.split(' ',maxsplit = 1))
             content.pop(0)
-            msg = ('(@%s) {0.author.mention} asked to encode ```%s``` in %s. Output:\n```%s```' % (msgtime, *content, channel, complexciphercore.convert(*content,'encode'))).format(message)
+            msg = ('(@%s) {0.author.mention} asked to encode ``` %s ``` in %s. Output:\n``` %s ```' % (msgtime, *content, channel, complexciphercore.convert(*content,'encode'))).format(message)
             await client.send_message(client.get_channel(dest_channel),msg)
             print("Succesfully encoded.")
+            return
 
-        if int(content[0:(int(content[0]) + 1)]) in range(11,999999):
+        if int(content[0:(int(content[0]) + 1)]) in range(11,6999999):
             print("Ident as code to be decoded.")
-            msg = ('(@%s) {0.author.mention} said in %s:\n```%s```' % (msgtime, channel, complexciphercore.convert(content,'decode'))).format(message)
+            msg = ('(@%s) {0.author.mention} said in %s:\n``` %s ```' % (msgtime, channel, complexciphercore.convert(content,'decode'))).format(message)
             await client.send_message(client.get_channel(dest_channel),msg)
             print("Succesfully decoded.")
+            return
+
+        else:
+            print("Message not relevant.")
+            return
+
     except ValueError:
         print("Message not relevant.")
+        return
     except IndexError:
         print("Message not relevant.")
+        return
 
 @client.event
 async def on_ready():
     print('\nLogged in as')
     print(client.user.name)
     print(client.user.id)
+    print('Running ComplexCipher %s.' % complexciphercore.VERSION)
     print('------')
 
-client.run(TOKEN)
+client.run(token)
