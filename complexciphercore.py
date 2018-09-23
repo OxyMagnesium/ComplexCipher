bd4dee0eac1ec17f8060f3aa49adf7684d7bbb97
Om_G <im.omgupta@gmail.com>
2018-09-23 17:55:32 +0530

    Tweaked supported characters, extended offset range to maximum possible.

--- complexciphercore.py
+++ complexciphercore.py
@@ -1,16 +1,16 @@
 #Core function of ComplexCipher.
-VERSION = '1.4.3'
+VERSION = '1.4.4'
 
 def convert(text,type): #Main function.
     import random
-    dictionary = ['0' , '!' , 'a' , 'b' , '1' , 'c' , 'd' , '2' , 'e' , '*' , 'f' , ',' , 'g' , '/' , 'h' , '3' , 'i' , 'j' , '.' , 'k' , 'l' , '4' , 'm' , ' ' , 'n' , '5' , 'o' , 'p' , ':' , 'q' , 'r' , '6' , 's' , '\"' , 't' , '\'' , 'u' , '-' , 'v' , '7' , 'w' , 'x' , '8' , 'y' , 'z' , '?' , '9']
+    dictionary = ['0' , '!' , 'a' , 'b' , '1' , 'c' , 'd' , '2' , 'e' , '(' , 'f' , ',' , 'g' , '/' , 'h' , '3' , 'i' , 'j' , '.' , 'k' , 'l' , '4' , 'm' , ' ' , 'n' , '5' , 'o' , 'p' , ':' , 'q' , 'r' , '6' , 's' , '-' , 't' , '\'' , 'u' , ')' , 'v' , '7' , 'w' , 'x' , '8' , 'y' , 'z' , '?' , '9']
 
     text = text.strip() #Setting up requirements for the algorithm.
     offset_included = 0
 
     if type == "encode" or type == "e": #Checking whether to encode or decode. If encoding, a random offset is added to the front of the string and used to offset the multiplier, increasing scrambling.
         random.seed()
-        randlist = [random.randint(1,9), random.randint(10,99), random.randint(100,999), random.randint(1000,9999), random.randint(10000,99999), random.randint(100000,999999)]
+        randlist = [random.randint(1,9), random.randint(10,99), random.randint(100,999), random.randint(1000,9999), random.randint(10000,99999), random.randint(100000,999999), random.randint(1000000,9999999), random.randint(10000000,99999999), random.randint(100000000,999999999)]
         offset = random.choice(randlist)
         offset_l = len(str(offset))
 
@@ -18,9 +18,9 @@ def convert(text,type): #Main function.
         output = "%s%s" % (str(offset_l),str(offset))
 
     elif type == "decode" or type == "d": #If decoding, the initial offset is retrieved from the front of the string and that character is skipped by the algorithm.
-        offset_l = int(text[0])
-        offset = int(text[1:(offset_l + 1)])
-        offset_included = offset_l + 1
+        offset_l = int(text[0]) + 1
+        offset = int(text[1:(offset_l)])
+        offset_included = offset_l
 
         mult = -1 * offset
         output = ""
