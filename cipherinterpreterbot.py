d0f1d4ec6b60e1aab9e2cbcc19ec7495b0cac77c
Om_G <im.omgupta@gmail.com>
2018-09-25 16:26:48 +0530

    Split the output into a separate line to make life easier on mobile.

--- cipherinterpreterbot.py
+++ cipherinterpreterbot.py
@@ -34,24 +34,28 @@ async def on_message(message):
     print("\n(@%s) Checking message by %s in %s..." % (msgtime, message.author, channel))
 
     if channel.is_private == True:
-        dest_channel = channel.id
+        dest_channel = client.get_channel(channel.id)
     else:
-        dest_channel = server_channel
+        dest_channel = client.get_channel(server_channel)
 
     try:
         if content.startswith('!e'): #Check for encode request.
             print("Ident as encode request.")
             content = (content.split(' ',maxsplit = 1))
             content.pop(0)
-            msg = ('(@%s) {0.author.mention} asked to encode \n` %s `\nin %s. Output:\n` %s `' % (msgtime, *content, channel, complexciphercore.convert(*content,'encode'))).format(message)
-            await client.send_message(client.get_channel(dest_channel),msg)
+            msg = ('(@%s) {0.author.mention} asked to encode ` %s ` in %s: ' % (msgtime, *content, channel)).format(message)
+            output = '``` %s ```' % complexciphercore.convert(*content, 'encode')
+            await client.send_message(dest_channel, msg)
+            await client.send_message(dest_channel, output)
             print("Succesfully encoded.")
             return
 
         if int(content[0:(int(content[0]) + 1)]) in range(11,9999999999): #Check for encoded text.
             print("Ident as code to be decoded.")
-            msg = ('(@%s) {0.author.mention} said in %s:\n` %s `' % (msgtime, channel, complexciphercore.convert(content,'decode'))).format(message)
-            await client.send_message(client.get_channel(dest_channel),msg)
+            msg = ('(@%s) {0.author.mention} said in %s: ' % (msgtime, channel)).format(message)
+            output = '``` %s ```' % complexciphercore.convert(content, 'decode')
+            await client.send_message(dest_channel,msg)
+            await client.send_message(dest_channel, output)
             print("Succesfully decoded.")
             return
 
