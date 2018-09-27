fc9923cbf77b3848e37c7784ce63795e3c00cb88
Om_G <im.omgupta@gmail.com>
2018-09-27 20:29:59 +0530

    Added support for multiple servers, more comments, other small changes.

--- cipherinterpreterbot.py
+++ cipherinterpreterbot.py
@@ -7,9 +7,10 @@ import time
 logging.basicConfig(level=logging.INFO)
 
 token = '' #Manually add token here.
-server_channel = '491938711693426688' #secret-codes
+servers = {}
+channels = {}
 
-if token == '':
+if token == '': #Get token if it's not already in the code.
     try:
         file = open('token.txt')
         token = file.read()
@@ -19,11 +20,32 @@ if token == '':
         end_stop = input("Press enter to quit.")
         quit()
 
+file = open('config.txt') #Get list of serviced servers and their corresponding IDs.
+section = ''
+for line in file:
+    line = line.strip()
+    if line.startswith('[') and line.endswith(']'):
+        section = line[1:-1]
+    line = line.split('=', maxsplit = 1)
+    try:
+        if section == 'SERVERS':
+            name = line[0].strip()
+            id = (line[1].split('"'))[1]
+            servers[id] = name
+        if section == 'CHANNELS':
+            name = line[0].strip()
+            id = (line[1].split('"'))[1]
+            channels[name] = id
+    except IndexError:
+        pass
+file.close()
+print("Servers and channels initialized.\nservers = %s\nchannels = %s\n" % (servers, channels))
+
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    gmtime = time.gmtime()
+    gmtime = time.gmtime() #Get message parameters.
     msgtime = '%s:%s:%s UTC' % (gmtime.tm_hour, gmtime.tm_min, gmtime.tm_sec)
     content = message.content
     channel = message.channel
@@ -31,15 +53,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    print("\n(@%s) Checking message by %s in %s..." % (msgtime, message.author, channel))
-
-    if channel.is_private == True:
+    if channel.is_private == True: #Ascertain message destination.
+        server = 'DM'
         dest_channel = client.get_channel(channel.id)
     else:
-        dest_channel = client.get_channel(server_channel)
+        server = servers[message.server.id]
+        dest_channel = client.get_channel(channels[server])
+
+    print("\n(@%s) Checking message by %s in %s: %s..." % (msgtime, message.author, server, channel))
 
     try:
-        if content.startswith('!e'): #Check for encode request.
+        if content.startswith('!e') or content.startswith('~e'): #Check for encode request.
             print("Ident as encode request.")
             content = (content.split(' ',maxsplit = 1))
             content.pop(0)
@@ -54,7 +78,7 @@ async def on_message(message):
             print("Ident as code to be decoded.")
             msg = ('(@%s) {0.author.mention} said in %s: ' % (msgtime, channel)).format(message)
             output = '``` %s ```' % complexciphercore.convert(content, 'decode')
-            await client.send_message(dest_channel,msg)
+            await client.send_message(dest_channel, msg)
             await client.send_message(dest_channel, output)
             print("Succesfully decoded.")
             return
@@ -63,10 +87,7 @@ async def on_message(message):
             print("Message not relevant.")
             return
 
-    except ValueError: #The method used to check for encoded text throws errors if it turns out not to be a code, hence this.
-        print("Message not relevant.")
-        return
-    except IndexError:
+    except (ValueError, IndexError): #The method used to check for encoded text throws errors if it turns out not to be a code, hence this.
         print("Message not relevant.")
         return
 
